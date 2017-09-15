656163ec077931eacce2ae9deba801d9ea9c4752
Adit Vijay <Administrator@DIN86000537.corp.capgemini.com>
2017-09-15 10:29:14 +0530

    Project added

---
+++ Project/A.java
@@ -0,0 +1,16 @@
+class A{
+public static void main(String args){
+
+system.output.println("Welcome to GITHUB");
+
+
+system.output.println("Welcome to Master");
+
+system.output.println("Welcome to DevOne");
+system.output.println("Welcome to Tester");
+
+
+
+}
+
+}
---
+++ Project/Tester.java
@@ -0,0 +1,13 @@
+class Tester{
+public static void main(String args){
+
+system.output.println("Welcome to GITHUB");
+
+
+system.output.println("Welcome to Tester");
+
+
+
+}
+
+}
